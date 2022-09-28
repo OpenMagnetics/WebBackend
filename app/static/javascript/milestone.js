469e7a8e8614500fc0c8b16369f672be1f67600d
AlfVII <Alfonso_VII@hotmail.com>
2022-09-28 21:58:58 +0200

    Added color for done tasks

--- app/static/javascript/milestone.js
+++ app/static/javascript/milestone.js
@@ -57,20 +57,29 @@ export default {
         },
         badge_color_style() {
             if (this.section == "Core losses") {
-                return "background-color: SlateGrey; color: black";
+                return "background-color: SlateGrey; color: var(--bs-dark)";
             }
             else if (this.section == "Gap Reluctance") {
-                return "background-color: MediumAquaMarine; color: black";
+                return "background-color: MediumAquaMarine; color: var(--bs-dark)";
             }
             else if (this.section == "Magnetic Design") {
-                return "background-color: Gold; color: black";
+                return "background-color: Gold; color: var(--bs-dark)";
+            }
+
+        },
+        card_class() {
+            if (this.state == "To Do") {
+                return "bg-primary";
+            }
+            else {
+                return "bg-info";
             }
 
         }
     },
     template: `             
         <div class="col-md-6 col-lg-4 col-xl-3 d-flex align-items-stretch">
-            <div class="card mt-4 box-shadow bg-primary">
+            <div class="card mt-4 box-shadow" :class=card_class>
                 <div class="card-header bg-transparent row mb-2">
                     <h4 class="text-secondary col-2">[[index + 1]]</h4>
                     <h4 class="text-dark col-7 font-size-lg">[[title]]</h4>
@@ -83,10 +92,13 @@ export default {
                     <p class="card-text text-start text-dark mb-auto mx-2">[[description]]</p>
                     <img v-if="imgSrc != null" class="card-img-right flex-auto d-none d-lg-block" :alt="imgAlt" style="object-fit: cover; width: 150px; max-height: 150px;" :src="imgSrc">
                 </div>
-                <div class="card-footer bg-transparent">
+                <div v-if="state == 'To Do'" class="card-footer bg-transparent">
                     <p class="text-end text-dark my-0">Current votes: [[numberVotes]]
                         <button :disabled="alreadyVoted" :class=btn_class @click="vote">[[alreadyVoted? "Already voted" : "Vote feature!"]]</button>
                     </p>
+                </div>
+                <div v-else class="card-footer bg-transparent">
+                    <span style="background-color: var(--bs-info); color: var(--bs-secondary); font-size: 20px" class="badge rounded-pill text-wrap ">[[state]]</span>
                 </div>
             </div>
         </div>
